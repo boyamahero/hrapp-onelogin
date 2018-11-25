ae8791c586c61f1cb6f4b155a921b6eec14aaad9
EGAT <egat@EGATs-MacBook-Pro-2.local>
2018-11-26 00:09:24 +0700

    แก้ไขหน้าเมนูสถิติ

--- src/pages/Statistic.vue
+++ src/pages/Statistic.vue
@@ -1,12 +1,21 @@
 <template>
-  <q-page padding>
-    <q-list highlight>
-      <q-list-header>ข้อมูลสถิติทางด้านสารสนเทศบุคคล</q-list-header>
-      <q-item-separator />
-      <q-item v-for="(report , key) in reports" :key="key" link :to="report.path">
-        <q-item-main :label="report.name"/>
-      </q-item>
-    </q-list>
+  <q-page>
+    <div class="row justify-center">
+      <div class="col-md-9 col-xs-12">
+        <q-card class="q-ma-md">
+          <q-card-main class="bg-light-blue-1">
+            <p class="header text-bold">ข้อมูลสถิติทางด้านสารสนเทศบุคคล</p>
+          </q-card-main>
+
+          <q-list highlight>
+          <q-item v-for="(report , key) in reports" :key="key" link :to="report.path">
+            <q-item-main :label="report.name"/>
+          </q-item>
+          </q-list>
+
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
