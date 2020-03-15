94d2f4367456f81a80f19ab90f3567f65ddffa8a
Tri Boonrit <triboonrit@gmail.com>
2020-03-16 00:26:45 +0700

    [geolct] login and register update

--- app/Http/Controllers/GeoLocationController.php
+++ app/Http/Controllers/GeoLocationController.php
@@ -2,11 +2,13 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
+use App\GeoLocation;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 class GeoLocationController extends Controller
 {
     public function insert(Request $request){
-        return $request;
+        // return $request;
+         return GeoLocation::all();
     }
 }
