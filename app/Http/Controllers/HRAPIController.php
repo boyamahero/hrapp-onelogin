a357e5e644af77f3512a61b1360432ff417682f4
EGAT <egat@EGATs-MacBook-Pro-2.local>
2020-05-30 17:28:50 +0700

    add permission personal data

--- app/Http/Controllers/HRAPIController.php
+++ app/Http/Controllers/HRAPIController.php
@@ -22,6 +22,28 @@ class HRAPIController extends Controller
             ]
         );
         $body = $response->getBody();
-        return response()->json(json_decode((string) $body));
+        $json = json_decode($body);
+
+        foreach ($json->data as $person) {
+            unset($person->person_id);
+            unset($person->person_type);
+            unset($person->senoir_number);
+            if (!$request->user()->can('view_personal_data_citizen_id'))
+                unset($person->person_id_code);
+            if (!$request->user()->can('view_personal_data_religion'))
+                unset($person->person_religion);
+            if (!$request->user()->can('view_personal_data_birthdate'))
+                unset($person->person_birthday);
+            if (!$request->user()->can('view_personal_data_health')) {
+                unset($person->person_blood_group);
+                unset($person->person_weight);
+                unset($person->person_height);
+            }
+        }
+
+        // var_export($json);
+        // if $json['data'] 
+
+        return response()->json($json);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
 /*
@@ -14,50 +16,56 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
-Route::get('/flush-cache', function() {
+Route::get('/flush-cache', function () {
     $exitCode = Artisan::call('modelCache:clear');
+    $exitCode = Artisan::call('php artisan cache:forget spatie.permission.cache');
+    return 'Done';
+});
+
+Route::get('/flush-cache-permission', function () {
+    $exitCode = Artisan::call('php artisan cache:forget spatie.permission.cache');
     return 'Done';
 });
 
 
-Route::get('/geolocation/{room}', function($room) {
+Route::get('/geolocation/{room}', function ($room) {
     return view('geolocation');
 });
 
-Route::get('/login', function() {
+Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/register/{type}', function($type) {
-    if($type=="person"){
+Route::get('/register/{type}', function ($type) {
+    if ($type == "person") {
         return view('register_person');
-    }else{
+    } else {
         return view('register_empegat');
     }
 });
 
-Route::post('register_save/{type}','GeoLocationController@insert');
+Route::post('register_save/{type}', 'GeoLocationController@insert');
 
-Route::get('/geolocation_result/{room}/{lat}/{long}', function($room,$lat,$long) {
-   
-$datalocate = array(
-	["code"=> "000000","roomname"=> "สำนักงานกลาง อาคาร ท.100 ห้อง 396"],
-    ["code"=> "002","roomname"=> "383 ท.100"]);
-    
-$user_ip = Request::ip();
-echo "IP: ".$user_ip; 
-echo "<br>";
-echo "Room: ".$datalocate[0]["roomname"]; 
-echo "<br>";
-echo "Date: ".date('Y-m-d');
-echo "<br>";
-echo "Time: ".date('H:i:s');
-echo "<br><br>";
-echo "Location: ".$lat.",".$long;
+Route::get('/geolocation_result/{room}/{lat}/{long}', function ($room, $lat, $long) {
 
+    $datalocate = array(
+        ["code" => "000000", "roomname" => "สำนักงานกลาง อาคาร ท.100 ห้อง 396"],
+        ["code" => "002", "roomname" => "383 ท.100"]
+    );
+
+    $user_ip = Request::ip();
+    echo "IP: " . $user_ip;
+    echo "<br>";
+    echo "Room: " . $datalocate[0]["roomname"];
+    echo "<br>";
+    echo "Date: " . date('Y-m-d');
+    echo "<br>";
+    echo "Time: " . date('H:i:s');
+    echo "<br><br>";
+    echo "Location: " . $lat . "," . $long;
 });
 
 // Redirect all to the front-end router
-Route::get('/{pattern?}', function() {
+Route::get('/{pattern?}', function () {
     return view('welcome');
 })->where('pattern', '.*');
