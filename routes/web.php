36b1b7d922a4158666857fd13b266576dd4cc104
BoyThanakorn <thanakornjato@gmail.com>
2020-10-02 19:17:37 +0700

    delete fn covid tracking

--- routes/web.php
+++ routes/web.php
@@ -26,44 +26,6 @@ Route::get('/flush-cache-permission', function () {
     return 'Done';
 });
 
-
-Route::get('/geolocation/{room}', function ($room) {
-    return view('geolocation');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/register/{type}', function ($type) {
-    if ($type == "person") {
-        return view('register_person');
-    } else {
-        return view('register_empegat');
-    }
-});
-
-Route::post('register_save/{type}', 'GeoLocationController@insert');
-
-Route::get('/geolocation_result/{room}/{lat}/{long}', function ($room, $lat, $long) {
-
-    $datalocate = array(
-        ["code" => "000000", "roomname" => "สำนักงานกลาง อาคาร ท.100 ห้อง 396"],
-        ["code" => "002", "roomname" => "383 ท.100"]
-    );
-
-    $user_ip = Request::ip();
-    echo "IP: " . $user_ip;
-    echo "<br>";
-    echo "Room: " . $datalocate[0]["roomname"];
-    echo "<br>";
-    echo "Date: " . date('Y-m-d');
-    echo "<br>";
-    echo "Time: " . date('H:i:s');
-    echo "<br><br>";
-    echo "Location: " . $lat . "," . $long;
-});
-
 // Redirect all to the front-end router
 Route::get('/{pattern?}', function () {
     return view('welcome');
